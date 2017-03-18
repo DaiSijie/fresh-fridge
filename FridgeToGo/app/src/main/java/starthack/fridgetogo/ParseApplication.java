47ee4e80ed32146f132466bbd5d962f17515e914
raphael strebel <raphael.strebel@epfl.ch>
2017-03-18 04:39:38 +0100

    started to work on parse

---
+++ FridgeToGo/app/src/main/java/starthack/fridgetogo/ParseApplication.java
@@ -0,0 +1,22 @@
+package starthack.fridgetogo;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.interceptors.ParseLogInterceptor;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // set applicationId, and server server based on the values in the Heroku settings.
+        // clientKey is not needed unless explicitly configured
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("myAppId") // should correspond to APP_ID env variable
+                .clientKey(null)  // set explicitly unless clientKey is explicitly configured on Parse server
+                .addNetworkInterceptor(new ParseLogInterceptor())
+                .server("https://my-parse-app-url.herokuapp.com/parse/").build());
+    }
+}
