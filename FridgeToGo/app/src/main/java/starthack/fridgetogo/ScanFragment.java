6e202c62be5cbf59e13f6d8dd522682e00ee7876
raphael strebel <raphael.strebel@epfl.ch>
2017-03-18 22:58:16 +0100

    upgraded scanFragment

--- FridgeToGo/app/src/main/java/starthack/fridgetogo/ScanFragment.java
+++ FridgeToGo/app/src/main/java/starthack/fridgetogo/ScanFragment.java
@@ -4,12 +4,14 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import starthack.fridgetogo.com.google.zxing.integration.android.IntentIntegrator;
 
@@ -26,6 +28,9 @@ public class ScanFragment extends Fragment implements OnClickListener {
 
 
     private Button scanBtn;
+    private Button plusButton;
+    private Button minusButton;
+    private Button okButton;
     private Spinner spinner;
 
     public ScanFragment() {
@@ -50,6 +55,15 @@ public class ScanFragment extends Fragment implements OnClickListener {
             IntentIntegrator scanIntegrator = new IntentIntegrator(getActivity());
             scanIntegrator.initiateScan();
         }
+        if(view.getId()==R.id.plus_button){
+            Log.d("fff", "plus was pressed");
+        }
+        if(view.getId()==R.id.minus_button){
+            Log.d("fff", "minus was pressed");
+        }if(view.getId()==R.id.ok_button){
+            Log.d("fff", "ok was pressed");
+        }
+
     }
 
     @Override
@@ -63,8 +77,19 @@ public class ScanFragment extends Fragment implements OnClickListener {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         scanBtn = (Button)getView().findViewById(R.id.scan_button);
-
         scanBtn.setOnClickListener(this);
+
+        TextView text = (TextView)getView().findViewById(R.id.scan_content);
+        text.setText("11/02/17");
+
+        plusButton = (Button)getView().findViewById(R.id.plus_button);
+        plusButton.setOnClickListener(this);
+
+        minusButton = (Button)getView().findViewById(R.id.minus_button);
+        minusButton.setOnClickListener(this);
+
+        okButton = (Button)getView().findViewById(R.id.ok_button);
+        okButton.setOnClickListener(this);
 
     }
 
